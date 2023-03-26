72a092b9324aae7a526d8535cb3f25fbc26a6a64
Richard Hagen <richard.hagen@gmail.com>
2023-03-26 12:30:45 +0200

    Added joke MVC

--- Web/Data/ApplicationDbContext.cs
+++ Web/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Web.Models;
 
 namespace Web.Data;
 
@@ -9,4 +10,5 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<Web.Models.Joke> Joke { get; set; } = default!;
 }
---
+++ Web/Models/Joke.cs
@@ -0,0 +1,12 @@
+namespace Web.Models;
+
+public class Joke
+{
+    public int Id { get; set; }
+    public string Question { get; set; }
+    public string Answer { get; set; }
+
+    public Joke()
+    {
+    }
+}
